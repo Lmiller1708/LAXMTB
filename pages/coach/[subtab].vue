5e5ea393d1ba09790a46af71738af66ecf5c8ac0
Lmiller1708 <Lmiller1708@gmail.com>
2026-09-11 17:50:23 -0500

    fix(router): add subtab and catchall pages to prevent 404 on /race/:slug/coach/pre-ride

---
+++ pages/coach/[subtab].vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+// Route: /coach/:subtab
+</script>
+
+<template>
+  <div />
+</template>
---
+++ pages/coach/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+// Route: /coach
+</script>
+
+<template>
+  <div />
+</template>
